9687d115f5b1182d6858d9b9d1a6d14efb07e327
gundamzaku <gundamzaku@163.com>
2016-10-11 19:46:37 +0800

    SESSION的小错误

--- web/protected/views/layouts/header_nonav.php
+++ web/protected/views/layouts/header_nonav.php
@@ -3,10 +3,10 @@
 		<div class="site site-header dark">
 			<a href="<?=Yii::app()->url->getIndexUrl()?>" id="header-logo"></a>
 			<div id="header-links">
-			<?php if(empty($this->session["usernname"])){?>
+			<?php if(empty($this->session["username"])){?>
 			<a href="<?=Yii::app()->url->getLoginUrl()?>">登录</a>
 			<?php }else{?>
-				<?=$this->session["usernname"]?>|<a href="<?=Yii::app()->url->getLoginOutUrl()?>">退出</a>
+				<?=$this->session["username"]?>|<a href="<?=Yii::app()->url->getLoginOutUrl()?>">退出</a>
 			<?php }?>
 			</div>
 		</div>
